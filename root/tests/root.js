9aa0557fdda283d4dc9808c24c3e3905c921941a
Efimov Maxim <maxim.efimov@deferit.com>
2022-07-04 21:20:18 +0700

    add create and increment counter program, update tests

--- root/tests/root.js
+++ root/tests/root.js
@@ -1,13 +1,53 @@
+const assert = require("assert");
 const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
 
 describe("root", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  /*
+   * Create and set a Provider
+   * */
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.Root;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.Root;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it('Creates a counter', async () => {
+    /*
+     * Call the create function via RPC
+     * */
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    await program.rpc.create({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+
+    /*
+     * Fetch the account and check the value of count
+     * */
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    
+    console.log({ count: account.count.toString() });
+
+    assert.ok(account.count.toString() == 0);
+    _baseAccount = baseAccount;
+  });
+
+  it('Increments the counter', async () => {
+    const baseAccount = _baseAccount;
+
+    await program.rpc.increment({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+
+    console.log({ count: account.count.toString() });
+    assert.ok(account.count.toString() == 1);
   });
 });
